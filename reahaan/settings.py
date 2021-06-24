7d2b283a69bd8f472e6002791f921ff16800e433
Reahaan Sheriff <reahaansherif@gmail.com>
2021-06-24 23:42:34 +0530

    modified

--- reahaan/settings.py
+++ reahaan/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 from pathlib import Path
 from decouple import config
-import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -151,5 +151,3 @@ STATIC_ROOT=os.path.join(BASE_DIR,'assets')
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
-# Activate Django-Heroku.
-django_heroku.settings(locals())
